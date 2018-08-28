25b1b08e9d0f8d999e0aa27ce3c2d6fb8e2a803f
Patrick Banguka <patrickbanguka@YanMac.local>
2018-08-28 04:55:13 +0200

    Executable

--- target/generated-sources/annotations/com/mycompany/affiliatemapping/entities/AffiliateGroupMap_.java
+++ target/generated-sources/annotations/com/mycompany/affiliatemapping/entities/AffiliateGroupMap_.java
@@ -1,15 +1,14 @@
 package com.mycompany.affiliatemapping.entities;
 
-import java.util.Date;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-08-28T01:06:50")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-08-28T04:54:35")
 @StaticMetamodel(AffiliateGroupMap.class)
 public class AffiliateGroupMap_ { 
 
-    public static volatile SingularAttribute<AffiliateGroupMap, Date> affiliateGroupId;
+    public static volatile SingularAttribute<AffiliateGroupMap, Integer> affiliateGroupId;
     public static volatile SingularAttribute<AffiliateGroupMap, Integer> id;
     public static volatile SingularAttribute<AffiliateGroupMap, Integer> affiliateId;
 
--- target/generated-sources/annotations/com/mycompany/affiliatemapping/entities/Affiliates_.java
+++ target/generated-sources/annotations/com/mycompany/affiliatemapping/entities/Affiliates_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-08-28T01:06:50")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-08-28T04:54:35")
 @StaticMetamodel(Affiliates.class)
 public class Affiliates_ { 
 
